7fe83c88397cd2df53f710377a3f06374b4e6c7a
gemabintang3108-prog <gemabintang3108@gmail.com>
2026-01-16 03:45:34 +0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyBY4i8moa3g2VUw_N7yfv8aYhB51xR65fs",
+  authDomain: "lawboys-c0f81.firebaseapp.com",
+  projectId: "lawboys-c0f81",
+  storageBucket: "lawboys-c0f81.firebasestorage.app",
+  messagingSenderId: "29582673064",
+  appId: "1:29582673064:web:781e159c5fdca7cf72ae9d"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
